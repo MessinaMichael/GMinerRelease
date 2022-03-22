53e52acb462409153c43778c4c2da31ff747500d
MessinaMichael <88297143+MessinaMichael@users.noreply.github.com>
2022-03-22 13:13:29 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -16,7 +16,7 @@ echo "Start changing memory transfer rate"
 
 for c in 0 1 2 3 4 5 6; do
     if [ $c = 1 ] || [ $c = 3 ]; then
-       sudo nvidia-settings -a [gpu:$c]/GPUMemoryTransferRateOffset[4]=2600
+       sudo nvidia-settings -a [gpu:$c]/GPUMemoryTransferRateOffset[4]=2400
        echo "changed GPU $c"
     else
     	sudo nvidia-settings -a [gpu:$c]/GPUMemoryTransferRateOffset[4]=2400
@@ -30,7 +30,7 @@ read -p "Press enter to continue"
 
 cd gminer_2_70_linux64/
 
-./miner --algo ethash --server us-eth.2miners.com:2020 --user bc1qcqc4pcngpwsapl6lt3k9w3ztxjtrelvfq7pwa6.powpowstrong
+./miner --algo ethash --server us-eth.2miners.com:2020 --user bc1qcqc4pcngpwsapl6lt3k9w3ztxjtrelvfq7pwa6.powpowstrong5
 
 
 
